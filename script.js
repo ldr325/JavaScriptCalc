2fd77c8967523a42b1415a1010cbce61711d04f4
LeoR <ldr325@nyu.edu>
2020-04-07 15:22:42 -0400

    fonts

--- script.js
+++ script.js
@@ -14,13 +14,14 @@ var fortuneButton = document.getElementById("fortuneButton");
 
 fortuneButton.addEventListener("click",fortune);
 
+var output = document.getElementById("fortune-output");
+
 function fortune() {
-  var fortunes = [" Nothing lost is nothing gained", " A fresh start will put you on your way", " A new perspective will come with the new year", " Listen to the cat", " A pleasant surprise is waiting for you"];
+  var fortunes = [" Nothing lost is nothing gained", " A fresh start will put you on your way", " A new perspective will come with the new year", " Listen to the cat", " A pleasant surprise is waiting for you", " Jack fit on the door", " Feed the cat or suffer"];
 
   var randFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
 
   var nameInput = document.getElementById("fortune-input").value;
-  var output = document.getElementById("fortune-output");
 
   if(nameInput != ""){
     output.innerHTML = randFortune;
@@ -30,3 +31,15 @@ function fortune() {
     alert("Enter a name dummy");
   }
 }
+
+function restyle(){
+  var color = ["red","brown","yellow","green","blue"];
+  var colorRand = color[Math.floor(Math.random() * color.length)];
+
+  var sizeRand = Math.floor(Math.random() * 100);
+
+  document.body.style.backgroundColor = colorRand;
+  output.style.fontSize = sizeRand + "px"
+}
+
+fortuneButton.addEventListener("click",restyle);
